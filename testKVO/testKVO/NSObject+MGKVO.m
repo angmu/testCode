696868801d77ca711d29510c3d5af9382607e2bb
muliang001 <anglimu@163.com>
2018-03-27 18:17:33 +0800

    自己完善kvo

--- testKVO/testKVO/NSObject+MGKVO.m
+++ testKVO/testKVO/NSObject+MGKVO.m
@@ -7,7 +7,8 @@
 //
 
 #import "NSObject+MGKVO.h"
-#import <objc/runtime.h>
+#include <objc/message.h>
+
 
 @implementation NSObject (MGKVO)
 
@@ -30,6 +31,10 @@
     // 动态绑定属性
     // 最终还是要调用_相应方法
     
+    
+    // --- 保存观察者
+    objc_setAssociatedObject(self, @"observer", observer, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    
 }
 
 // 把隐含参数补齐，就可以接收name了
@@ -37,10 +42,58 @@
 // 子类的setName方法
 void setName(id self, SEL _cmd, id name)
 {
-    
-    NSLog(@"setName---调用了----%@", name);
+    NSLog(@"NSObject+MGKVO---setName---调用了----%@", name);
     
     // 调用父类方法，消息机制
-    [super setName:name];
+//    [super setName:name];
+//    objc_msgSendSuper([self class], _cmd, name);
+    
+    
+    
+    // ------1
+    // 保存当前类
+//    Class MyClass = [self class];
+//    // 将self指向父类，准备调用父类方法
+//    object_setClass(self, class_getSuperclass([self class]));
+//    // 调用父类方法
+//    objc_msgSend(self, @selector(setName:), name);
+//    // 通知观察者
+//    id observer = objc_getAssociatedObject(self, @"observer");
+//    objc_msgSend(observer, @selector(observeValueForKeyPath:ofObject:change:context:), @"name", self, nil, nil);
+//    // 改回子类
+//    object_setClass(self, MyClass);
+    // 5监听到<MGPerson: 0x60000000a2d0>属性的改变为(null)
+    /**
+     
+     2018-03-27 18:03:01.760025+0800 testKVO[6590:1043259] <MGPerson: 0x600000013fb0>监听到name属性的改变为{
+     kind = 1;
+     new = 5;
+     old = lisa;
+     }
+     2018-03-27 18:03:01.760218+0800 testKVO[6590:1043259] name的值------5
+
+     --------------------------
+     NSObject+MGKVO---setName---调用了----7
+     <MGPerson: 0x600000015400>监听到name属性的改变为(null)
+     name的值------7
+     */
+    
+    // ------2 直接调用父类方法，设置name的值
+    // class_getSuperclass(object_getClass(self));
+    // Use of undeclared identifier 'super'
+//    objc_msgSendSuper(super, @selector(setName:), name);
+    
+    
+    
+    
 }
+
+
+/**
+ 1. 扩展性
+ 2. block实现kvo函数式编程
+ 
+    创建的子类就没有这个name属性
+ 
+ */
 @end
--- testKVO/testKVO/ViewController.m
+++ testKVO/testKVO/ViewController.m
@@ -15,10 +15,17 @@
 @interface ViewController ()
 
 @property (nonatomic, strong) MGPerson *p;
-
 @property (nonatomic, copy) NSString *address;
 
 @end
+
+/**
+ [iOS开发-KVO的奥秘](https://www.jianshu.com/p/742b4b248da9)
+ [KVO原理及自定义KVO](https://blog.csdn.net/u014247354/article/details/78403567)
+ [手动实现KVO](https://www.cnblogs.com/oc-bowen/p/5999564.html)
+ [对于KVO，你真的了解么？](https://www.jianshu.com/p/a32d851603e5)
+ 
+ */
 
 @implementation ViewController
 
@@ -44,21 +51,22 @@
     _p = p;
 
     [p MG_addObserver:self forKeyPath:@"name" options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld context:nil];
+//    [p addObserver:self forKeyPath:@"name" options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld context:nil];
 }
 
 
 // 监听方法
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context
 {
-    NSLog(@"object-----%@", object);
-    NSLog(@"-----%@", change);
+    NSLog(@"%@监听到%@属性的改变为%@", object, keyPath, change);
 }
 
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     NSString *randomStr = [NSString stringWithFormat:@"%d", arc4random()%10];
-    _p.name = randomStr;
+    self.p.name = randomStr;
+    NSLog(@"name的值------%@", self.p.name);
 }
 
 

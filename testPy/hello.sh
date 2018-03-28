28780164765e1913549bbada64b5ceee9f0d21e2
mul001 <467603639@qq.com>
2018-03-28 08:02:20 +0800

    解决冲突

---
+++ testPy/hello.sh
@@ -0,0 +1,8 @@
+name='I have a dream'
+echo ${name:2:3}
+echo ${#name}
+echo ${name#I*a}
+echo ${name#*a}
+echo ${name##I*a}
+echo ${name}
+
---
+++ testPy/hello2.sh
@@ -0,0 +1,6 @@
+# hello
+name='穆良'
+echo $name
+name=2
+echo $name
+echo "hello world"
